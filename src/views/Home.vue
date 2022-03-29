c12ec4970fc2ecc93cd8f573dc4c023f8b4d2cb0
masudachika <masuda@builwing.com>
2022-03-29 17:39:36 +0900

    天気予報アプリ実装

---
+++ src/views/Home.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="home flex">
+    <Card v-for="(data,index) in lists" :key="index" :city="data.city"/>
+  </div>
+</template>
+
+<script>
+import Card from "../components/Card";
+
+export default {
+  data() {
+    return {
+      lists: [
+        {
+          city: "sapporo"
+        },
+        {
+          city: "tokyo"
+        },
+        {
+          city: "nagoya"
+        },
+        {
+          city: "osaka"
+        },
+        {
+          city: "fukuoka"
+        },
+        {
+          city: "naha"
+        },
+      ]
+    };
+  },
+  components: {
+    Card
+  }
+};
+</script>
+
+<style scoped>
+.flex {
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+}
+
+@media screen and (max-width: 768px) {
+  .flex {
+    display: block;
+    justify-content: center;
+  }
+}
+</style>
